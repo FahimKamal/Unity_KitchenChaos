ad7d422cb5fc45a6d044e252d30f1515c547f091
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2023-05-30 14:46:53 +0600

    working on player movement script

---
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    
+    private void Update()
+    {
+        var inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1;
+        }
+        
+        // Normalized inputVector for smooth player movement 
+        inputVector = inputVector.normalized;
+
+        var moveDir = new Vector3(inputVector.x, 0.0f, inputVector.y);
+
+        // Move player
+        transform.position += moveDir * (moveSpeed * Time.deltaTime);
+
+        // Rotate player
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime);
+    }
+    
+}
