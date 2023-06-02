1af6625ef66841310e7b6627437af1f7c0d91d84
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2023-06-02 16:22:48 +0600

    New input system added and operational.

--- KitchenChaos/Assets/Scripts/GameInput.cs
+++ KitchenChaos/Assets/Scripts/GameInput.cs
@@ -1,33 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace GameScripts
 {
     public class GameInput : MonoBehaviour
     {
+        private PlayerInputActions _playerInputActions;
+        private void Awake()
+        {
+            _playerInputActions = new PlayerInputActions();
+            _playerInputActions.Player.Enable();
+        }
+
         public Vector2 GetMovementVectorNormalized()
         {
-            var inputVector = Vector2.zero;
-            if (Input.GetKey(KeyCode.W))
-            {
-                inputVector.y = 1;
-            }
 
-            if (Input.GetKey(KeyCode.S))
-            {
-                inputVector.y = -1;
-            }
+            #region code for old input system
 
-            if (Input.GetKey(KeyCode.A))
-            {
-                inputVector.x = -1;
-            }
+            // var inputVector = Vector2.zero;
+            // if (Input.GetKey(KeyCode.W))
+            // {
+            //     inputVector.y = 1;
+            // }
+            //
+            // if (Input.GetKey(KeyCode.S))
+            // {
+            //     inputVector.y = -1;
+            // }
+            //
+            // if (Input.GetKey(KeyCode.A))
+            // {
+            //     inputVector.x = -1;
+            // }
+            //
+            // if (Input.GetKey(KeyCode.D))
+            // {
+            //     inputVector.x = 1;
+            // }
 
-            if (Input.GetKey(KeyCode.D))
-            {
-                inputVector.x = 1;
-            }
+            #endregion
+            
+            var inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
 
             // Normalized inputVector for smooth player movement 
             inputVector = inputVector.normalized;
@@ -35,3 +47,5 @@ namespace GameScripts
         }
     }
 }
+
+
