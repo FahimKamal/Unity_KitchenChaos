5843cad7cc50faccd5cd0bca43000d603bc8200b
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2023-06-04 12:32:52 +0600

    kitchen objects can be spawn on top of counters now. Different types of objects now can be stored in scriptable objects.

--- KitchenChaos/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -4,9 +4,16 @@ namespace GameScripts
 {
     public class ClearCounter : MonoBehaviour
     {
+        [SerializeField] private KitchenObjectSO _kitchenObjectSo;
+        [SerializeField] private Transform counterTopPoint;
         public void Interact()
         {
             Debug.Log($"Interacted with {gameObject.name}");
+            
+            var kitchenObjectTransform = Instantiate(_kitchenObjectSo.kitchenObjectPrefab, counterTopPoint);
+            kitchenObjectTransform.localPosition = Vector3.zero;
+            
+            Debug.Log($"Spawned object: {kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().kitchenObjectName}");
         }
     }
 }
