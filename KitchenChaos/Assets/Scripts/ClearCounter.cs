4908fe92a1d0d0bebf740d1dc47823b3945481a9
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2023-06-04 17:41:20 +0600

    working on parent system.

--- KitchenChaos/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -6,14 +6,30 @@ namespace GameScripts
     {
         [SerializeField] private KitchenObjectSO _kitchenObjectSo;
         [SerializeField] private Transform counterTopPoint;
+        [SerializeField] private ClearCounter secondClearCounter;
+        
+        /// <summary>
+        /// Place to know what kind of kitchen object is setting on top of this counter. 
+        /// </summary>
+        private KitchenObject _kitchenObject;
+        
         public void Interact()
         {
             Debug.Log($"Interacted with {gameObject.name}");
+
+            if (_kitchenObject == null)
+            {
+                var kitchenObjectTransform = Instantiate(_kitchenObjectSo.kitchenObjectPrefab, counterTopPoint);
+                kitchenObjectTransform.localPosition = Vector3.zero;
             
-            var kitchenObjectTransform = Instantiate(_kitchenObjectSo.kitchenObjectPrefab, counterTopPoint);
-            kitchenObjectTransform.localPosition = Vector3.zero;
-            
-            Debug.Log($"Spawned object: {kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().kitchenObjectName}");
+                Debug.Log($"Spawned object: {kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().kitchenObjectName}");
+                _kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+                _kitchenObject.ClearCounter = this;
+            }
+            else
+            {
+                Debug.Log($"Already have object: {_kitchenObject.GetKitchenObjectSO().kitchenObjectName}");
+            }
         }
     }
 }
--- KitchenChaos/Assets/Scripts/KitchenObject.cs
+++ KitchenChaos/Assets/Scripts/KitchenObject.cs
@@ -7,6 +7,16 @@ namespace GameScripts
     public class KitchenObject : MonoBehaviour
     {
         [SerializeField] private KitchenObjectSO kitchenObjectSO;
+        private ClearCounter clearCounter;
+
+        /// <summary>
+        /// Place to know which counter is now holding this kitchen object.
+        /// </summary>
+        public ClearCounter ClearCounter
+        {
+            get { return clearCounter; }
+            set { clearCounter = value; }
+        }
 
         public KitchenObjectSO GetKitchenObjectSO() => kitchenObjectSO;
     }
