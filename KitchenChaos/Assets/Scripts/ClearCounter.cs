c0136cea5b1bd815dcdbaaba4cdc988f60257f1b
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2023-06-06 00:11:48 +0600

    player-counter interaction complete.

--- KitchenChaos/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace GameScripts
@@ -7,12 +8,24 @@ namespace GameScripts
         [SerializeField] private KitchenObjectSO _kitchenObjectSo;
         [SerializeField] private Transform counterTopPoint;
         [SerializeField] private ClearCounter secondClearCounter;
-        
+        [SerializeField] private bool testing;
+
         /// <summary>
         /// Place to know what kind of kitchen object is setting on top of this counter. 
         /// </summary>
         private KitchenObject _kitchenObject;
-        
+
+        private void Update()
+        {
+            if (testing && Input.GetKeyDown(KeyCode.T))
+            {
+                if (_kitchenObject != null)
+                {
+                    _kitchenObject.SetClearCounter(secondClearCounter);
+                }
+            }
+        }
+
         public void Interact()
         {
             Debug.Log($"Interacted with {gameObject.name}");
@@ -20,16 +33,40 @@ namespace GameScripts
             if (_kitchenObject == null)
             {
                 var kitchenObjectTransform = Instantiate(_kitchenObjectSo.kitchenObjectPrefab, counterTopPoint);
-                kitchenObjectTransform.localPosition = Vector3.zero;
-            
-                Debug.Log($"Spawned object: {kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().kitchenObjectName}");
-                _kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
-                _kitchenObject.ClearCounter = this;
+                kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+
+                Debug.Log(
+                    $"Spawned object: {kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().kitchenObjectName}");
             }
             else
             {
                 Debug.Log($"Already have object: {_kitchenObject.GetKitchenObjectSO().kitchenObjectName}");
             }
+        }
+        
+        public Transform GetkitchenObjectFollowTransform()
+        {
+            return  counterTopPoint;
+        }
+
+        public void SetKitchenObject(KitchenObject kitchenObject)
+        {
+            this._kitchenObject = kitchenObject;
+        }
+
+        public KitchenObject GetKitchenObject()
+        {
+            return _kitchenObject;
+        }
+
+        public void ClearKitchenObject()
+        {
+            _kitchenObject = null;
+        }
+
+        public bool HasKitchenObject()
+        {
+            return _kitchenObject != null;
         }
     }
 }
--- KitchenChaos/Assets/Scripts/KitchenObject.cs
+++ KitchenChaos/Assets/Scripts/KitchenObject.cs
@@ -9,13 +9,27 @@ namespace GameScripts
         [SerializeField] private KitchenObjectSO kitchenObjectSO;
         private ClearCounter clearCounter;
 
-        /// <summary>
-        /// Place to know which counter is now holding this kitchen object.
-        /// </summary>
-        public ClearCounter ClearCounter
+        public void SetClearCounter(ClearCounter counter)
         {
-            get { return clearCounter; }
-            set { clearCounter = value; }
+            if (clearCounter != null)
+            {
+                clearCounter.ClearKitchenObject();
+            }
+            clearCounter = counter;
+
+            if (clearCounter.HasKitchenObject())
+            {
+                Debug.LogError("Counter already has a kitchenObject.");
+            }
+            clearCounter.SetKitchenObject(this);
+
+            transform.parent = clearCounter.GetkitchenObjectFollowTransform();
+            transform.localPosition = Vector3.zero;
+        }
+
+        public ClearCounter GetClearCounter()
+        {
+            return clearCounter;
         }
 
         public KitchenObjectSO GetKitchenObjectSO() => kitchenObjectSO;
